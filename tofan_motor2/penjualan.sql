e5d802c142571c1cce02e653cadc554255efa040
afniamalia <afniamalia459@yahoo.com>
2018-01-04 19:49:39 +0700

    penambahan field gambar pada database

--- tofan_motor2/penjualan.sql
+++ tofan_motor2/penjualan.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.6.5.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 15, 2017 at 03:08 PM
--- Server version: 10.1.26-MariaDB
--- PHP Version: 7.1.9
+-- Generation Time: 04 Jan 2018 pada 13.18
+-- Versi Server: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -25,37 +23,39 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `data_barang`
+-- Struktur dari tabel `data_barang`
 --
 
 CREATE TABLE `data_barang` (
   `kode_barang` varchar(10) NOT NULL,
-  `nama_barang` varchar(20) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `gambar_barang` varchar(30) NOT NULL,
   `supplier` varchar(15) NOT NULL,
   `harga` int(15) NOT NULL,
   `satuan` varchar(5) NOT NULL,
   `stok` int(5) NOT NULL,
   `keterangan` varchar(15) NOT NULL,
-  `tanggal` datetime NOT NULL
+  `tanggal` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `data_barang`
+-- Dumping data untuk tabel `data_barang`
 --
 
-INSERT INTO `data_barang` (`kode_barang`, `nama_barang`, `supplier`, `harga`, `satuan`, `stok`, `keterangan`, `tanggal`) VALUES
-('asd003', 'ban', 'PT.AHM', 3000, 'ltr', 75, 'tersedia', '2017-12-14 00:00:00'),
-('asd004', 'gear', 'PT.Suzuki', 40000, 'pcs', 1, 'tersedia', '2017-12-14 00:00:00'),
-('asd009', 'shock', 'PT.AHM', 40000, 'pcs', 40, 'tersedia', '2017-12-14 00:00:00'),
-('asd01', 'oli', 'PT.ASPIRA', 40000, 'pcs', 0, 'tersedia', '2017-12-14 00:00:00'),
-('asdjas', 'asd', 'PT.ASPIRA', 88, 'jasgd', 8287, 'sjgs', '2017-12-14 23:27:40'),
-('asgdja', 'ajsgdj', 'PT.ASPIRA', 723467, 'hsg', 27846, 'kjashkd', '2017-12-14 23:29:13'),
-('baru', 'Battery', 'PT.ASPIRA', 140000, 'pcs', 20, 'tersedia', '2017-12-14 00:00:00');
+INSERT INTO `data_barang` (`kode_barang`, `nama_barang`, `gambar_barang`, `supplier`, `harga`, `satuan`, `stok`, `keterangan`, `tanggal`) VALUES
+('01-B21-317', 'LEGSHIELD DALAM SUPRA', 'leg supra.jpg', 'PT.AHM', 142000, 'ST', 15, '<p>-</p>', '2018-01-01'),
+('01-B42-327', 'COVER BODY VARIO 110', 'cover vario.jpg', 'PT.AHM', 146000, 'ST', 50, '<p>-</p>', '2018-01-01'),
+('01-D23-317', 'LEGSHIELD DALAM JUPITER', 'leg jupi.jpg', 'PT.AHM', 131700, 'ST', 100, '<p>-</p>', '2018-01-01'),
+('BN-CRS-01', 'BAN CORSA 70/90-17', 'corsa.jpg', 'CORSA', 182000, 'PCS', 10, '<p>TUBELESS</p>', '2018-01-01'),
+('BN-FDR-01', 'BAN FDR 70/90-17', 'FDR.jpg', 'FDR', 183000, 'PCS', 15, '<p>TUBELESS</p>', '2018-01-01'),
+('kd-001', 'Ban', 'ban.jpg', 'PT.AHM', 145000, '-', 5, 'tersedia', '2017-12-14'),
+('kd-005', 'Oli', 'oli.jpg', 'PT.AHM', 40000, '-', 5, 'tersedia', '2017-12-14'),
+('kd-006', 'Shock', 'shock.jpg', 'PT.AHM', 40000, 'pcs', 25, 'tersedia', '2017-12-14');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `data_barang_keluar`
+-- Struktur dari tabel `data_barang_keluar`
 --
 
 CREATE TABLE `data_barang_keluar` (
@@ -68,18 +68,19 @@ CREATE TABLE `data_barang_keluar` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `data_barang_keluar`
+-- Dumping data untuk tabel `data_barang_keluar`
 --
 
 INSERT INTO `data_barang_keluar` (`no`, `tgl_keluar`, `nama_barang`, `jumlah`, `harga`, `total_harga`) VALUES
-(107, '2017-12-14', 'oli', 60, 40000, 2400000),
-(108, '2017-12-14', 'ban', 37, 3000, 111000),
-(118, '2017-12-14', 'gear', 14, 40000, 560000);
+(138, '2018-01-03', 'COVER BODY VARI', 1000, 146000, 146000000),
+(139, '2018-01-03', 'Ban', 75, 145000, 10875000),
+(143, '2018-01-03', 'Gear', 20, 100000, 2000000),
+(144, '2018-01-04', 'Shock', 3, 40000, 120000);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `data_supplier`
+-- Struktur dari tabel `data_supplier`
 --
 
 CREATE TABLE `data_supplier` (
@@ -89,18 +90,24 @@ CREATE TABLE `data_supplier` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `data_supplier`
+-- Dumping data untuk tabel `data_supplier`
 --
 
 INSERT INTO `data_supplier` (`kode_supplier`, `nama_supplier`, `kontak`) VALUES
-('AD002', 'PT.ASPIRA', '112'),
-('as001', 'PT.AHM', '0001233'),
-('SP-00', 'PT.Suzuki', '003515560987');
+('1', 'PT.AHM', '-'),
+('10', 'PT.AHAY', '-'),
+('2', 'PT.SUZUKI', '-'),
+('3', 'PT.ASPIRA', '-'),
+('4', 'CORSA', '-'),
+('5', 'FDR', '-'),
+('7', 'IRC', '-'),
+('8', 'WIN', '-'),
+('9', 'PT.YAMAHA', '-');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `user`
 --
 
 CREATE TABLE `user` (
@@ -111,11 +118,15 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user`
+-- Dumping data untuk tabel `user`
 --
 
 INSERT INTO `user` (`id_user`, `nama`, `username`, `password`) VALUES
-(11, 'eko', 'prastyo', '123');
+(11, 'eko', 'prastyo', '123'),
+(12, 'Kelompok', 'kelompok4', '123'),
+(13, 'Firmansyah', 'firman', '123'),
+(14, 'samsul', 'nugie', '12345678'),
+(15, 'asdasd', 'anjay', '123');
 
 --
 -- Indexes for dumped tables
@@ -153,15 +164,12 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `data_barang_keluar`
 --
 ALTER TABLE `data_barang_keluar`
-  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=119;
-
+  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=145;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-COMMIT;
-
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
